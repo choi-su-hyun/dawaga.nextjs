56da10a7e116a1c306dbff8e7c3472582eb4c3ed
최수현 <choi45769@gmail.com>
2024-10-05 22:22:34 +0900

    feat :: 버튼 컴포넌트 제작

---
+++ src/components/common/Button/Button.tsx
@@ -0,0 +1,40 @@
+import { NextPage } from "next";
+import { ButtonHTMLAttributes, ReactNode } from "react";
+import style from "./Button.module.scss";
+
+type ButtonVariantType = "cta-btn" | "ghost-btn" | "text-center";
+type ButtonSizeType = "size-small";
+
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: string;
+  variant?: ButtonVariantType; //버튼 종류
+  size?: ButtonSizeType; //버튼 크기
+  rightSlot?: ReactNode; //버튼 오른쪽에 넣는 아이콘
+  isFullWidth?: boolean; //전체 크기 버튼
+}
+
+const Button: NextPage<Props> = ({
+  // const Button: React.FC<Props> = ({
+  children,
+  variant,
+  size,
+  rightSlot,
+  isFullWidth,
+  ...rest
+}) => {
+  return (
+    <>
+      <button
+        className={`${variant && style[variant]} ${size && style[size]} ${
+          isFullWidth ? style["full-width"] : ""
+        } ${style["btn-style"]}`}
+        {...rest}
+      >
+        {children}
+        {rightSlot}
+      </button>
+    </>
+  );
+};
+
+export default Button;
